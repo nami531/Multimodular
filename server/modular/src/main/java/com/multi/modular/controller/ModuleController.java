e5a7982ca5d69bfc57a39b5a7cd8280d210c8f68
Nadia Marquez <nadiamarquez27112005@gmail.com>
2025-02-17 18:03:05 +0100

    feat: Module Controller

--- server/modular/src/main/java/com/multi/modular/controller/ModuleController.java
+++ server/modular/src/main/java/com/multi/modular/controller/ModuleController.java
@@ -1,5 +1,11 @@
 package com.multi.modular.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org
+
+@RestController
 public class ModuleController {
-    
+    @Autowired
+    private ModuleService moduleService;
 }
