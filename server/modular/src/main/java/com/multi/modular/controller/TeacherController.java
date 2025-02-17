aef47a0081964ba162ca345d1a88da6075f8f461
Nadia Marquez <nadiamarquez27112005@gmail.com>
2025-02-17 18:12:40 +0100

    feat: Upload teacher files

--- server/modular/src/main/java/com/multi/modular/controller/TeacherController.java
+++ server/modular/src/main/java/com/multi/modular/controller/TeacherController.java
@@ -1,5 +1,34 @@
 package com.multi.modular.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.multi.modular.domain.Folder;
+import com.multi.modular.service.TeacherService;
+import com.multi.modular.service.utils.FileManagementService;
+
+@RestController
+@RequestMapping("/teachers")
 public class TeacherController {
-    
+
+    @Autowired
+    private TeacherService service;
+
+    @Autowired
+    private FileManagementService fileManagementService;
+    private final Folder FOLDER_NAME = Folder.TEACHER; 
+
+    @PostMapping(value = "/uploadFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> newElement(@RequestPart("file") MultipartFile file) {
+        fileManagementService.store(file, FOLDER_NAME.path );
+        
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
