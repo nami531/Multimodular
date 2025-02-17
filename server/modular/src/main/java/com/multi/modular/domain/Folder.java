540e29e1164ec349497ba4e7a121e4e476a6d846
Nadia Marquez <nadiamarquez27112005@gmail.com>
2025-02-17 18:01:10 +0100

    feat: Enum with asociated paths

---
+++ server/modular/src/main/java/com/multi/modular/domain/Folder.java
@@ -0,0 +1,10 @@
+package com.multi.modular.domain;
+// No lo asocio con barra por si utilizamos path.join 
+public enum Folder {
+    MODULE("modules"),
+    TEACHER("teachers"),
+    TEMPLATE("finalTemplates"); 
+
+    public final String path; 
+    Folder(String p) {path=p;}; 
+}
